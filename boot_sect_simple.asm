14df4b2cc027cc6057933c114a5a96d9327ef687
gamapg <gamapg@yahoo.com>
2023-04-28 23:53:37 +0800

    got my OS to say hello

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,16 +1,8 @@
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
-int 0x10
+; Infinite loop (e9 fd ff)
+loop:
+    jmp loop 
 
-jmp $ ; jump to current address = infinite loop
-
-; padding and magic number
-times 510 - ($-$$) db 0
+; Fill with 510 zeros minus the size of the previous code
+times 510-($-$$) db 0
+; Magic number
 dw 0xaa55 
